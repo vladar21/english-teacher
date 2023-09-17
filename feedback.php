a8e0dc302de880942079162dec39028e7d4b0940
vladar21 <vladar21@gmail.com>
2023-09-17 22:02:53 +0100

    feat: add feadback handler

--- feedback.php
+++ feedback.php
@@ -1,12 +1,47 @@
 <?php
-$to = "vlad.rastvorov@aol.com"; // The email address you want to receive the test email
-$subject = "Test email from PHP"; // The email subject
-$message = "This is a test email to check if PHP mail() function works"; // The email body
-$headers = "From: webmaster@example.com"; // The email sender
 
-if (mail($to, $subject, $message, $headers)) {
-  echo "Test email sent";
+header("Content-Type: application/json");
+
+// Check if the form is submitted
+// Check if the form is submitted
+if (isset($_POST['submit'])) {
+  // Get the user's input from the form
+  $name = htmlspecialchars($_POST['name_feedback']); // Escape user input to prevent XSS
+  $email = filter_var($_POST['email_feedback'], FILTER_SANITIZE_EMAIL); // Sanitize user input to prevent header injection
+  $feedback = htmlspecialchars($_POST['message_feedback']); // Escape user input to prevent XSS
+
+  // Validate the user's input
+  // You can add more validation rules here
+  if (empty($name) || empty($email) || empty($feedback)) {
+    // Display an error message if any field is empty
+    // Use json_encode to return a JSON object with status and message properties
+    echo json_encode(array("status" => "error", "message" => "Please fill in all the fields."));
+  } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    // Display an error message if the email is not valid
+    // Use json_encode to return a JSON object with status and message properties
+    echo json_encode(array("status" => "error", "message" => "Please enter a valid email address."));
+  } else {
+    // Prepare the email headers and body
+    $to = "vlad.rastvorov@aol.com"; // The email address you want to receive the feedback
+    $subject = "Feedback from $name"; // The email subject
+    $message = "Name: $name\nEmail: $email\nFeedback: $feedback"; // The email body
+    $headers = "From: office@improve-english.com.ua"; // The email sender (use your own domain to prevent email spoofing)
+    $headers .= "\r\nReply-To: $email"; // The email reply-to address
+
+    // Send the email using the mail() function
+    if (mail($to, $subject, $message, $headers)) {
+      // Display a success message if the email is sent
+      // Use json_encode to return a JSON object with status and message properties
+      echo json_encode(array("status" => "success", "message" => "Thank you for your feedback. We appreciate it."));
+    } else {
+      // Display an error message if the email is not sent
+      // Use json_encode to return a JSON object with status and message properties
+      echo json_encode(array("status" => "error", "message" => "Sorry, something went wrong. Please try again later."));
+    }
+  }
 } else {
-  echo "Failed to send";
+  // Display an error message if the form is not submitted
+  // Use json_encode to return a JSON object with status and message properties
+  echo json_encode(array("status" => "error", "message" => "Please submit the form."));
 }
 ?>
